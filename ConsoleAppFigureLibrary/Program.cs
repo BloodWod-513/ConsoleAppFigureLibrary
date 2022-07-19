711a7710cc4ddb75bdc6cbea321e1c255ecd127c
BloodWod-513 <dayn_2013@mail.ru>
2022-07-19 18:49:30 +0500

    add two triangles for print

--- ConsoleAppFigureLibrary/Program.cs
+++ ConsoleAppFigureLibrary/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FigureLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppFigureLibrary
 {
-    internal class Program
+    public class Program
     {
+        static void Main()
+        {
+            Triangle triangle = new Triangle(new Vector(0, 0), new Vector(1, 2), new Vector(2, 0));
+            Console.WriteLine(triangle.Print());
+            Triangle straightAngleTriangle = new Triangle(new Vector(0, 0), new Vector(0, 2), new Vector(5, 0));
+            Console.WriteLine(straightAngleTriangle.Print());
+            Console.ReadKey();
+        }
     }
 }
