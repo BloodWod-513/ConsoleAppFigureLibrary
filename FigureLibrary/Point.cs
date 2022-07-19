50a861504ab5089c79fbb76b45e41c390b8f0475
BloodWod-513 <dayn_2013@mail.ru>
2022-07-19 17:38:06 +0500

    add Point

---
+++ FigureLibrary/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureLibrary
+{
+    public class Vector
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public Vector(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+        public static Vector operator +(Vector pointA, Vector pointB)
+        {
+            return new Vector(pointA.X + pointB.X, pointA.Y + pointB.Y);
+        } 
+        public static Vector operator -(Vector pointA, Vector pointB)
+        {
+            return new Vector(pointA.X - pointB.X, pointA.Y - pointB.Y);
+        }
+        public float Length()
+        {
+            return (float)Math.Sqrt(X * X + Y * Y);
+        }
+        public float Distance(Vector point)
+        {
+            return (this - point).Length();
+        }
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
